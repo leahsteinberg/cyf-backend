fd5138b52fbd00ce6300e64b321ee06468c504f2
Leah Steinberg <leahsteinberg@Leahs-MacBook-Air-2.local>
2025-10-01 08:41:40 -0700

    working state, want to add phone number next

--- app.ts
+++ app.ts
@@ -1,12 +1,28 @@
 import {auth} from './auth.ts';  
 import express from 'express';
 import { toNodeHandler, fromNodeHeaders } from "better-auth/node"; 
-
+import twilio from 'twilio';
 import cors from 'cors';
-  // const express = require('express');
-  // const cors = require('cors');
+import { phoneNumber } from 'better-auth/plugins';
+
   const app = express();
   const port = 3000;
+
+//   const accountSid = process.env.TWILIO_ACCOUNT_SID;
+//   const authToken = process.env.TWILIO_AUTH_TOKEN;
+//   console.log({accountSid, authToken});
+//   const client = twilio(accountSid, authToken);
+
+// client.messages
+//           .create({
+//             to: recipientPhoneNumber,
+//             from: twilioPhoneNumber,
+//             body: messageBody,
+//           })
+//           .then(message => console.log(`Message sent with SID: ${message.sid}`))
+//           .catch(error => console.error(`Error sending message: ${error}`));
+
+
 
   app.use(cors({
     origin: 'http://localhost:8081'
@@ -36,6 +52,7 @@ import cors from 'cors';
     return res.json(data);
   });
 
+
     app.post("/api/signInEmail", async (req, res) => {
     const session = await auth.api.signInEmail({
         body: {
@@ -45,8 +62,36 @@ import cors from 'cors';
         },
         headers: fromNodeHeaders(req.headers),
       });
-      console.log("signed in???--", session);
+      //console.log("signed in???--", session);
     return res.json(session);
+  });
+
+
+      app.post("/api/signInPhone", async (req, res) => {
+    const session = await auth.api.signInPhoneNumber({
+        body: {
+          phoneNumber: req.body.phoneNumber,
+          password: req.body.password,
+          rememberMe: true,
+        },
+        headers: fromNodeHeaders(req.headers),
+      });
+      //console.log("signed in???--", session);
+    return res.json(session);
+  });
+
+    app.post("/api/addContacts", async (req, res) => {
+    console.log(req.body);
+    // const session = await auth.api.signInEmail({
+    //     body: {
+    //       email: req.body.email,
+    //       password: req.body.password,
+    //       rememberMe: true,
+    //     },
+    //     headers: fromNodeHeaders(req.headers),
+    //   });
+      //console.log("signed in???--", session);
+    return res.json({});
   });
 
   app.get('/', (req, res) => {
@@ -56,3 +101,5 @@ import cors from 'cors';
   app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
   });
+
+
