503dc6e8221f4f0919cbeffe70a079bf0cb13c8d
Leah Steinberg <leahsteinberg@Leahs-MacBook-Air-2.local>
2025-11-17 10:58:24 -0800

    Response from register-push endpoint

--- endpoint-handlers/push-handler.ts
+++ endpoint-handlers/push-handler.ts
@@ -3,5 +3,6 @@ export const handlePush = async (req: Request, res: Response) => {
     const pushToken = '';
     const userId = '';
     console.log("register push --", req.body)
+    res.send("GOT THE PUSH TOKEN!")
 
 }
