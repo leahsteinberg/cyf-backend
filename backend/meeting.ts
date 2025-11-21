a354bd889a8c2f3b1a0e1b3ed9408b0978b0b089
Leah Steinberg <leahsteinberg@Leahs-MacBook-Air-2.local>
2025-11-21 13:31:49 -0800

    delete all broadcasted meetings

--- backend/meeting.ts
+++ backend/meeting.ts
@@ -1,16 +1,18 @@
 
-import type { MeetingState,  Meeting, User } from '../types.js';
-import { prisma } from './auth.js';  
+import type { Meeting } from '../types.js';
 import { getMeetingOffers } from './offer.js';
 import { clearOutOffers } from './process-meeting.js';
+import { deleteMeeting } from './update/meeting-update.js';
 
 
 export const findBroadcastedMeetings = (meetings: Meeting[]): Meeting[] => {
     return meetings.filter(meeting => meeting.meetingType === 'BROADCAST');
 }
 
-
 export const deleteBroadcastedMeeting = async (meeting: Meeting) => {
-    const offers = await getMeetingOffers({meetingId: meeting.id})
-    await clearOutOffers(offers)
+    await deleteMeeting({meetingId: meeting.id});
+    // const offers = await getMeetingOffers({meetingId: meeting.id})
+
+    // deleteMeeting
+    // await clearOutOffers(offers)
 }
