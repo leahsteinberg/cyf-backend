9cb9128614258cafa2647fa935c29b4c0fd94392
Leah Steinberg <leahsteinberg@Leahs-MacBook-Air-2.local>
2025-12-12 15:12:23 -0800

    Don't create offers for accepted drafts - YET

--- endpoint-handlers/meeting-handler.ts
+++ endpoint-handlers/meeting-handler.ts
@@ -174,7 +174,8 @@ export const handleAcceptDraftMeeting = async (req: Request, res: Response) => {
     }
 
     // Create offers for the activated meeting
-    await processOfferForNewMeeting(activatedMeeting);
+    // NOT YET IMPLEMENTED -- WILL NEED TO DO THIS CAREFULLY AND WITH NEW LOGIC!!
+    //await processOfferForNewMeeting(activatedMeeting);
 
     console.log("Draft meeting accepted and activated:", meetingId);
     res.json({
