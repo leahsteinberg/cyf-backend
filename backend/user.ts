7af1d596f78051128a49e5583f58e1babbe42ce2
Leah Steinberg <leahsteinberg@Leahs-MacBook-Air-2.local>
2025-12-15 00:03:55 -0800

    unused import

--- backend/user.ts
+++ backend/user.ts
@@ -1,6 +1,5 @@
 import type { RequestHandler } from 'express';
 import { auth } from './auth.js';
-import { fromNodeHeaders } from 'better-auth/node';
 
 export const createUser = async ({ email, phoneNumber, name, password }: { email: string, phoneNumber: string, name: string, password: string }) => {
     const user = await auth.api.signUpEmail({
