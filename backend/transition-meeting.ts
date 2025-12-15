a1e5af937ff04c85f977c93b1b07100c05f66ac7
Leah Steinberg <leahsteinberg@Leahs-MacBook-Air-2.local>
2025-12-15 13:06:58 -0800

    Validation logic for state transitions

--- backend/transition-meeting.ts
+++ backend/transition-meeting.ts
@@ -1,5 +1,6 @@
-import { ACCEPTED_MEETING_STATE, ACCEPTOR_ACTOR_ROLE, CANCELED_MEETING_STATE, DISMISSED_SUGGESTION_MEETING_STATE, DRAFT_MEETING_STATE, EXPIRED_MEETING_STATE, FRIEND_SPECIFIC_TARGET_TYPE, INITIATOR_ACTOR_ROLE, OPEN_TARGET_ACTOR_ROLE, OPEN_TARGET_TYPE, PAST_MEETING_STATE, REJECTED_MEETING_STATE, SEARCHING_MEETING_STATE, SPECIFIC_TARGET_ACTOR_ROLE, type DomainEvent, type Meeting, type MeetingActorRole, type MeetingState } from "../types.js";
+import { ACCEPTED_MEETING_STATE, ACCEPTOR_ACTOR_ROLE, CANCELED_MEETING_STATE, DISMISSED_SUGGESTION_MEETING_STATE, DRAFT_MEETING_STATE, EXPIRED_MEETING_STATE, FRIEND_SPECIFIC_TARGET_TYPE, INITIATOR_ACTOR_ROLE, OPEN_TARGET_ACTOR_ROLE, OPEN_TARGET_TYPE, PAST_MEETING_STATE, REJECTED_MEETING_STATE, SEARCHING_MEETING_STATE, SPECIFIC_TARGET_ACTOR_ROLE, SYSTEM_ACTOR_ROLE, type DomainEvent, type Meeting, type MeetingActorRole, type MeetingState } from "../types.js";
 import { getMeetingById } from "./query/meeting-lookup.js";
+import { isTimePast } from "./utils.js";
 
 
 const idempotentStates = new Set([
@@ -10,6 +11,40 @@ const idempotentStates = new Set([
     ]
 );
 
+const isTransitionActorAllowed = (meeting: Meeting, toState: MeetingState, actorId: string):boolean => {
+    const meetingActorRole: MeetingActorRole = getMeetingActorRole({meeting, actorId});
+    return allowedRolesForGoalState[toState].includes(meetingActorRole);
+}
+
+const isTransitionAllowed = (fromState: MeetingState, toState: MeetingState): boolean => {
+    return allowedStateTransitions[fromState].includes(toState);
+}
+const isTimeValid = async (meeting: Meeting, toState: MeetingState): Promise<boolean> => {
+    const timeIsPast = await isTimePast({eventTime: meeting.scheduledEnd});
+    return timeIsPast ? (toState === PAST_MEETING_STATE) : (toState !== PAST_MEETING_STATE);
+}
+
+const allowedRolesForGoalState: Record<MeetingState, MeetingActorRole[]> = {
+    DISMISSED_SUGGESTION: [INITIATOR_ACTOR_ROLE],
+    DRAFT: [],
+    SEARCHING: [INITIATOR_ACTOR_ROLE],
+    ACCEPTED: [SPECIFIC_TARGET_ACTOR_ROLE, OPEN_TARGET_ACTOR_ROLE],
+    REJECTED: [SPECIFIC_TARGET_ACTOR_ROLE, OPEN_TARGET_ACTOR_ROLE],
+    EXPIRED: [SYSTEM_ACTOR_ROLE],
+    CANCELED: [SYSTEM_ACTOR_ROLE],
+    PAST: [SYSTEM_ACTOR_ROLE],
+}
+
+const allowedStateTransitions: Record<MeetingState, MeetingState[]> = {
+    DISMISSED_SUGGESTION: [],
+    DRAFT: [SEARCHING_MEETING_STATE, DISMISSED_SUGGESTION_MEETING_STATE],
+    SEARCHING: [ACCEPTED_MEETING_STATE, REJECTED_MEETING_STATE, CANCELED_MEETING_STATE],
+    ACCEPTED: [PAST_MEETING_STATE, CANCELED_MEETING_STATE],
+    REJECTED: [PAST_MEETING_STATE],
+    EXPIRED: [PAST_MEETING_STATE],
+    CANCELED: [PAST_MEETING_STATE],
+    PAST: [],
+} 
 
 const buildDomainEvents = ({fromState, toState, meeting}: {fromState: MeetingState, toState: MeetingState, meeting: Meeting} ) => {
 
@@ -17,12 +52,10 @@ const buildDomainEvents = ({fromState, toState, meeting}: {fromState: MeetingSta
 
 const getMeetingActorRole = ({meeting, actorId}: {meeting: Meeting, actorId: string}): MeetingActorRole => {
     const meetingCreatorId = meeting?.userFromId;
-    if (meeting.meetingState === PAST_MEETING_STATE
-        || meeting.meetingState === EXPIRED_MEETING_STATE || meeting.meetingState === ) {
-
-        }
-    
-    
+    const meetingState = meeting.meetingState;
+    if ([PAST_MEETING_STATE, EXPIRED_MEETING_STATE, REJECTED_MEETING_STATE].includes(meetingState)) {
+        return SYSTEM_ACTOR_ROLE;
+    }
     else if (actorId === meetingCreatorId && actorId !== meeting.acceptedUserId && actorId !== meeting.targetUserId) {
         return INITIATOR_ACTOR_ROLE;
     }
@@ -35,7 +68,6 @@ const getMeetingActorRole = ({meeting, actorId}: {meeting: Meeting, actorId: str
     else if (meeting.targetType === OPEN_TARGET_TYPE) {
         return OPEN_TARGET_ACTOR_ROLE;
     }
-
     throw new Error("This is user does not have a valid role for this meeting.")
 }
 
@@ -61,37 +93,17 @@ export const transitionMeeting = async ({meetingId, toState, actorId}: {meetingI
     if (!validStateTransition) {
         throw new Error(`Incorrect state transition from ${meetingState} to ${toState}`);
     }
-    const transitionActorValid = isTransitionActorAllowed(meeting, toState, actorId)
+    const transitionActorValid = isTransitionActorAllowed(meeting, toState, actorId);
+    if (!transitionActorValid) {
+        throw new Error(`Incorrect state transition from ${meetingState} to ${toState} for this user: ${actorId}`);
+    }
 
+    const timeConstraintsValid = await isTimeValid(meeting, toState);
+    if (!timeConstraintsValid) {
+        throw new Error(`Incorrect time status for transition from ${meetingState} to ${toState}.`);
+    }
+
+    return {meeting, events: []}
 };
 
-const isTransitionActorAllowed = (meeting: Meeting, toState: MeetingState, actorId: string):boolean => {
-    const meetingActorRole: MeetingActorRole = getMeetingActorRole({meeting, actorId});
 
-}
-
-const allowedRolesForGoalState: Record<MeetingState, MeetingActorRole[]> = {
-    DISMISSED_SUGGESTION: [INITIATOR_ACTOR_ROLE],
-    DRAFT: [],
-    SEARCHING: [INITIATOR_ACTOR_ROLE],
-    ACCEPTED: [SPECIFIC_TARGET_ACTOR_ROLE, OPEN_TARGET_ACTOR_ROLE],
-    REJECTED: [SPECIFIC_TARGET_ACTOR_ROLE, OPEN_TARGET_ACTOR_ROLE],
-    EXPIRED: [],
-    CANCELED: [],
-    PAST: [],
-}
-
-const allowedStateTransitions: Record<MeetingState, MeetingState[]> = {
-    DISMISSED_SUGGESTION: [],
-    DRAFT: [SEARCHING_MEETING_STATE, DISMISSED_SUGGESTION_MEETING_STATE],
-    SEARCHING: [ACCEPTED_MEETING_STATE, REJECTED_MEETING_STATE, CANCELED_MEETING_STATE],
-    ACCEPTED: [PAST_MEETING_STATE, CANCELED_MEETING_STATE],
-    REJECTED: [PAST_MEETING_STATE],
-    EXPIRED: [PAST_MEETING_STATE],
-    CANCELED: [PAST_MEETING_STATE],
-    PAST: [],
-} 
-
-const isTransitionAllowed = (fromState: MeetingState, toState: MeetingState): boolean => {
-    return allowedStateTransitions[fromState].includes(toState);
-}
