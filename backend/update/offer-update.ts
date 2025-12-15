ba3c2a61c895a2ad4cfde82a74bc5fa7b6e17f28
Leah Steinberg <leahsteinberg@Leahs-MacBook-Air-2.local>
2025-12-14 23:30:10 -0800

    Remove offer type - step 1

--- backend/update/offer-update.ts
+++ backend/update/offer-update.ts
@@ -9,15 +9,14 @@ import {
 } from '../../types.js';
 
 
-export const createOffer = async ({meetingId, userOfferedId, expiresAt, offerType}
-    : {meetingId: string, userOfferedId: string, expiresAt: Date, offerType: MeetingType}): Promise<Offer| undefined> => {
+export const createOffer = async ({meetingId, userOfferedId, expiresAt }
+    : {meetingId: string, userOfferedId: string, expiresAt: Date }): Promise<Offer| undefined> => {
     const offer = await prisma.offer.create({
         data: {
             meetingId,
             userOfferedId,
             offerState: OPEN_OFFER_STATE_TYPE,
             expiresAt,
-            offerType
         }
     })
     console.log("Meeting ID:" , meetingId, "Made a new offer: ", offer);
